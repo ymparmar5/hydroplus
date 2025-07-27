6625b98b0ed149054a29d2fe37259403af535c74
ymparmar5 <ymparmar7@gmail.com>
2025-07-27 21:27:02 +0530

    fix social side bar

--- src/Components/SocialSidebar.jsx
+++ src/Components/SocialSidebar.jsx
@@ -26,30 +26,43 @@ const socialLinks = [
     url: "https://wa.me/918000074088",
     label: "WhatsApp",
   },
-  {
-    icon: "/telecall.png",
-    url: "https://t.me/hydroplusinternational",
-    label: "Telegram",
-  },
-  {
-    icon: "/contact.png",
-    url: "tel:+918000074088",
-    label: "Call",
-  },
 ];
+
 
 const SocialSidebar = () => {
   return (
-    <div className="fixed right-1 top-1/2 -translate-y-1/2 z-40 flex flex-col gap-4">
-      {socialLinks.map((link, index) => (
-        <div
-          key={index}
-          className="bg-white shadow border border-orange-500 w-12 h-14 rounded-l-full flex items-center justify-center cursor-pointer"
-          onClick={() => window.open(link.url, link.url.startsWith('tel:') ? '_self' : '_blank')}
-        >
-          <img src={link.icon} alt={link.label} className="w-7 h-7 object-contain" />
-        </div>
-      ))}
+    <div
+      className="fixed top-1/2 right-0 -translate-y-1/2 z-[1000] 
+        sm:top-1/2 sm:right-0 sm:-translate-y-1/2
+        top-auto bottom-4 right-2 translate-y-0 sm:bottom-auto sm:right-0"
+    >
+      <ul className="flex flex-col items-end space-y-2">
+        {socialLinks.map((link, index) => (
+          <li key={index} className="group relative">
+            <a
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center bg-white border border-orange-500 rounded-l-full px-1 py-1 shadow-md 
+                w-6 h-6 hover:w-[140px] sm:w-[35px] sm:h-auto sm:hover:w-[160px] 
+                transition-all duration-300 overflow-hidden whitespace-nowrap"
+            >
+              <div className="w-5 h-5 flex items-center justify-center flex-shrink-0 sm:w-[35px] sm:h-[35px]">
+                <img
+                  src={link.icon}
+                  alt={link.label}
+                  className="w-[18px] h-[18px] object-contain sm:w-[25px] sm:h-[25px]"
+                />
+              </div>
+              <span
+                className="ml-2 text-xs opacity-0 group-hover:opacity-100 transition-opacity duration-200 delay-100 hidden sm:inline"
+              >
+                {link.label}
+              </span>
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
